88f98400ce5a7d4ff6441fe7b8439c759561064b
Ved Patil <vedpatil611@gmail.com>
2021-03-29 19:10:00 +0530

    Scene tree lib

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,7 @@ include "Dependencies/GLAD"
 IncludeDirs = {}
 IncludeDirs["GLFW"] = "Dependencies/GLFW/include"
 IncludeDirs["GLAD"] = "Dependencies/GLAD/include"
+IncludeDirs["SceneTree"] = "SceneTree/src"
 
 project "PotatoTiler"
 	location "PotatoTiler"
@@ -37,7 +38,43 @@ project "PotatoTiler"
 
 	links {
 		"GLFW",
-		"GLAD"
+		"GLAD",
+		"SceneTree"
+	}
+
+	files { 
+		"%{prj.name}/src/**.h", 
+		"%{prj.name}/src/**.cpp"
+	}
+
+	filter "system:windows"
+		defines {
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "RELEASE" }
+		optimize "On"
+		buildoptions "/MD"
+
+project "SceneTree"
+	location "SceneTree"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	systemversion "latest"
+
+	inlining "Auto"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
+
+	includedirs {
+		"%{prj.name}/src/",
 	}
 
 	files { 
