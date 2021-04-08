d50bdf272817c59abfd1222d67fd97044153f8f8
Ved Patil <vedpatil611@gmail.com>
2021-04-08 10:03:25 +0530

    Window key and mouse inputs and projection matrix

--- PotatoTiler/src/Window.cpp
+++ PotatoTiler/src/Window.cpp
@@ -2,6 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
 #include <stdexcept>
 
 Window::Window()
@@ -16,6 +17,7 @@ Window::Window()
         throw std::runtime_error("Failed to create window");
     }
 
+    glfwGetFramebufferSize(m_Window, &m_BufferWidth, &m_BufferHeight);
     glfwMakeContextCurrent(m_Window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -24,8 +26,12 @@ Window::Window()
         throw std::runtime_error("Failed to load opengl");
     }
 
+    m_Proj = glm::ortho(-m_BufferWidth / 2, m_BufferWidth / 2, -m_BufferHeight / 2, m_BufferHeight / 2);
+
     glfwSetWindowUserPointer(m_Window, this);
     glfwSetKeyCallback(m_Window, &Window::keyCallback);
+    glfwSetMouseButtonCallback(m_Window, &Window::mouseKeyCallback);
+    glfwSetCursorPosCallback(m_Window, &Window::mousePositionCallback);
 }
 
 Window::~Window()
@@ -55,6 +61,66 @@ bool Window::shouldClose() const
     return glfwWindowShouldClose(m_Window);
 }
 
-void Window::keyCallback(GLFWwindow* m_Window, int key, int scancode, int action, int mods)
+float Window::getXChange()
 {
+    float change = m_xChange;
+    m_xChange = 0;
+    return change;
+}
+
+float Window::getYChange()
+{
+    float change = m_yChange;
+    m_yChange = 0;
+    return change;
+}
+
+void Window::keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    Window* currentWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+        {
+            currentWindow->getKeys()[key] = true;
+        }
+        if (action == GLFW_RELEASE)
+        {
+            currentWindow->getKeys()[key] = false;
+        }
+    }
+}
+
+void Window::mouseKeyCallback(GLFWwindow* window, int key, int action, int mods)
+{
+    Window* currentWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+        {
+            currentWindow->getKeys()[key] = true;
+        }
+        if (action == GLFW_RELEASE)
+        {
+            currentWindow->getKeys()[key] = false;
+        }
+    }
+}
+
+void Window::mousePositionCallback(GLFWwindow* window, double xPos, double yPos)
+{
+    Window* currentWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    currentWindow->m_xChange = static_cast<float>(xPos) - currentWindow->m_LastX;
+    currentWindow->m_yChange = currentWindow->m_LastY - static_cast<float>(yPos);
+
+    currentWindow->m_LastX = static_cast<float>(xPos);
+    currentWindow->m_LastY = static_cast<float>(yPos);
 }
--- PotatoTiler/src/Window.h
+++ PotatoTiler/src/Window.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 class Window
 {
 private:
 	struct GLFWwindow* m_Window;
 	bool m_Keys[1024] = { 0 };
+	int m_BufferWidth, m_BufferHeight;
+	float m_LastX = 0.0f, m_LastY = 0.0f;
+	float m_xChange = 0.0f, m_yChange = 0.0f;
+	glm::mat4 m_Proj;
 public:
 	Window();
 	~Window();
@@ -16,5 +22,11 @@ public:
 	bool shouldClose() const;
 	bool* getKeys() { return m_Keys; }
 
+	float getXChange();
+	float getYChange();
+	glm::mat4 getProjMatrix() const { return m_Proj; }
+
 	static void keyCallback(GLFWwindow* m_Window, int key, int scancode, int action, int mods);
+	static void mouseKeyCallback(GLFWwindow* window, int key, int action, int mods);
+	static void mousePositionCallback(GLFWwindow* window, double xPos, double yPos);
 };
